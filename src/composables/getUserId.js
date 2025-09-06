111dbcdb39b6eed74bb17a1e0b98323b2161c252
chawrona <dawidchawrona@gmail.com>
2025-09-06 20:46:49 +0200

    fix

--- src/composables/getUserId.js
+++ src/composables/getUserId.js
@@ -1,8 +1,14 @@
 export default function getUserId() {
     let userId = localStorage.getItem("userId");
-
     if (!userId) {
-        userId = self.crypto.randomUUID();
+        userId = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+            /[xy]/g,
+            function (c) {
+                const r = (Math.random() * 16) | 0;
+                const v = c === "x" ? r : (r & 0x3) | 0x8;
+                return v.toString(16);
+            },
+        );
         localStorage.setItem("userId", userId);
     }
 
